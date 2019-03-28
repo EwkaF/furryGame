6a94d60d438c5e3a470e5890f3f6045bf86e8b4f
EwkaF <obarka@interia.pl>
2019-03-28 20:54:01 +0100

    formated

--- js/app.js
+++ js/app.js
@@ -1,39 +1,39 @@
 import { Game } from './game'
 
 document.addEventListener("DOMContentLoaded", function () {
-   
-document.querySelector('#start button').addEventListener('click',(e) => {
 
-    document.querySelector('#start').classList.add('invisible');
-    document.querySelector('#score').classList.remove('invisible');
-    document.querySelector('#board').classList.remove('invisible');
-    var game = new Game();
-    game.showFurry();
-    game.showCarrot();
-    game.showFoxInterval();
-    game.startGame();
-    
-    document.addEventListener('keydown', event => {
-        game.turnFurry(event);
-    });
+    document.querySelector('#start button').addEventListener('click', (e) => {
 
-})
+        document.querySelector('#start').classList.add('invisible');
+        document.querySelector('#score').classList.remove('invisible');
+        document.querySelector('#board').classList.remove('invisible');
+        var game = new Game();
+        game.showFurry();
+        game.showCarrot();
+        game.showFoxInterval();
+        game.startGame();
 
-document.querySelector('#over button').addEventListener('click',(e) => {
-    this.score = 0;
-    document.querySelector("#score strong").innerText = this.score;
-    document.querySelector('#over').classList.add('invisible');
-    var game = new Game();
-    game.showFurry();
-    game.showCarrot();
-    game.showFoxInterval();
-    game.startGame();
-    
-    document.addEventListener('keydown', event => {
-        game.turnFurry(event);
-    });
+        document.addEventListener('keydown', event => {
+            game.turnFurry(event);
+        });
 
-})
+    })
+
+    document.querySelector('#over button').addEventListener('click', (e) => {
+        this.score = 0;
+        document.querySelector("#score strong").innerText = this.score;
+        document.querySelector('#over').classList.add('invisible');
+        var game = new Game();
+        game.showFurry();
+        game.showCarrot();
+        game.showFoxInterval();
+        game.startGame();
+
+        document.addEventListener('keydown', event => {
+            game.turnFurry(event);
+        });
+
+    })
 
 
 
